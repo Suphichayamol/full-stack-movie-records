c1489f33488783719d8fb9128db17b4d879d943f
Suphichayamol <Suphichayamol.work@gmail.com>
2026-03-11 20:13:14 +0700

    add schema sql

---
+++ database/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO movies (title, year, rating) VALUES
+('Avatar', 2009, 'PG'),
+('Inception', 2010, 'PG'),
+('Interstellar', 2014, 'PG'),
+('The Dark Knight', 2008, 'PG'),
+('Titanic', 1997, 'PG'),
+('Avengers Endgame', 2019, 'PG'),
+('Joker', 2019, 'R'),
+('Deadpool', 2016, 'R');
